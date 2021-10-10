c9ed05514bdd9cfce3bf1447e5ec63e9332a8da4
notthatsmart <notthesmartest48@gmail.com>
2021-10-10 12:43:24 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import praw
+import time
+from keep_alive import keep_alive
+reddit = praw.Reddit(client_id="xx1xbGxR1hbDCe1NoXjxQQ",
+                     client_secret="b6w50lVs3-v87ywTToOWl8C2Ogxy7g",
+                     user_agent="<console:amazingbot:1.0",
+                     username="allbotsaregood",
+                     password="Amazingbob9")
+subreddit = reddit.subreddit("memes")
+keep_alive()
+
+for comment in subreddit.stream.comments():
+    if comment.author.name == reddit.user.me().name:
+        continue
+    if "!attack" in comment.body.lower():
+        print(f"https://reddit.com{comment.permalink}")
+        comment.reply('bad human')
+        comment.upvote()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
