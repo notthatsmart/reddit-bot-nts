c2c92410538611ae9aebae56e2108841284d96d3
notthatsmart <notthesmartest48@gmail.com>
2021-10-11 18:30:45 -0500

    very confused about the rate limit

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ reddit = praw.Reddit(client_id="NGbw1N6Q354uiLxF6Xtn3w",
                      user_agent="<console:amazingbot:2.0",
                      username="notthebottest",
                      password="Amazingbob9")
-subreddit = reddit.subreddit("All")
+subreddit = reddit.subreddit("kickopenthedoor")
 
 keep_alive()
 
 for comment in subreddit.stream.comments():
     if comment.author.name == reddit.user.me().name:
         continue
-    if "1984" in comment.body.lower():
+    if "weak" in comment.body.lower():
         print(f"https://reddit.com{comment.permalink}")
-        comment.reply('By george orwell 1949')
+        #comment.reply('By george orwell 1949')
         comment.upvote()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
