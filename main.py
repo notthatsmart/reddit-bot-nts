cdd92370f153c6a56631bf79e9a32717332fa7d7
notthatsmart <notthesmartest48@gmail.com>
2021-10-11 19:23:34 -0500

    fixed the problem that had botherd me for a weeek i fogot 1 letter in the client_secret

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import praw
 import time
 from keep_alive import keep_alive
 reddit = praw.Reddit(client_id="NGbw1N6Q354uiLxF6Xtn3w",
-                     client_secret="8nSWsQCK6dgdogx9AQFHjpFOYZQOa",
+                     client_secret="8nSWsQCK6dgdogx9AQFHjpFOYZQOaw",
                      user_agent="<console:amazingbot:2.0",
                      username="notthebottest",
                      password="Amazingbob9")
@@ -17,5 +17,17 @@ for comment in subreddit.stream.comments():
         print(f"https://reddit.com{comment.permalink}")
         #comment.reply('By george orwell 1949')
         comment.upvote()
+
+for post in subreddit.new(limit=1):
+    print("*************")
+    print(post.title)
+
+    for comment in post.comments:
+        if hasattr(comment, "body"):
+            comment_lower = comment.body.lower()
+            if " weak " in comment_lower:
+                print("-------------")
+                print(comment.body)
+                time.sleep(60)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
